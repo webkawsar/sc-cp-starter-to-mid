b1f47048ace47f7d4a23e5c8be6a9ec097e68875
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-12-23 19:48:25 +0600

    start cp practice

---
+++ index.js
@@ -0,0 +1,19 @@
+// Orientation Guideline
+// class --> Sat, Tues --> 9.30
+// contest --> 
+
+// Class - Saturday, Tuesday at 9:30 PM (almost 1:30 hour)
+// Contest - Monday, Friday at 8:30 PM (2:30 hour)
+// CodeChef - Wednesday at 8:30 PM (We will announce, when to participate)
+
+
+
+
+
+
+
+
+
+
+
+
