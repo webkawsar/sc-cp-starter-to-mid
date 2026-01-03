b0713cf7a2438d4cef6f5a739eea8623cb2142c3
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-03 11:58:57 +0600

    practice string varibale, user input

---
+++ string-variable.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char name[14] = "Kawsar Ahmed";
+    cout <<name<<endl;
+}
+
+
---
+++ user-input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char name;
+    int age;
+
+    cout<<"Please enter your name"<<endl;
+    cin>>name;
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+    cout<<"Your name is: " << name << " & you are " << age << "years old";
+}
+
+
+
