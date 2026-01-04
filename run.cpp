7c13a223f54c9426c7fb57b29dab768b8f8cc33f
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-04 23:51:44 +0600

    practic problem solving on codeforcess

---
+++ run.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+    cout << X << " - " << Y << " = " << X - Y << endl;
+}
---
+++ varibale.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a = 3;
+    double b = 100;
+
+    // int c = b / a;
+    double c = int(b / a);
+    
+    cout << c;
+}
