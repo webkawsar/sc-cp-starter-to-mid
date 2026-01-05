a19c070dea7dd287b2939a87925bfbb51b25bbd7
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-05 22:53:52 +0600

    solved problem A

---
+++ 1-say-hello-with-cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// A. Say Hello With C++
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << "Hello, " << s << endl;
+}
+
+
---
+++ problems/problems-link.cpp
@@ -0,0 +1,3 @@
+
+// Here is given all problem
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158
