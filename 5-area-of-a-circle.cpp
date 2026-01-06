c5b79309808a1e4b2c696652ff7a1699febf4f6b
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-06 23:56:35 +0600

    try to solve problem 5

---
+++ 5-area-of-a-circle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// E. Area of a Circle
+
+int main() {
+    //  consider π = 3.141592653.
+    // Area = π * R2. 
+
+    int r;
+    cin >> r;
+
+    float result = 3.141592653 * (r * r);
+    cout << result;
+}   
+
+
