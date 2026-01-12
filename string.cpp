0d67101b4d9d671130efa285c6772075565c4e62
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-12 20:28:39 +0600

    practice string related some functions

--- string.cpp
+++ string.cpp
@@ -21,17 +21,52 @@ int main() {
     // string fullName = firstName + " " + lastName;
     // cout << fullName << endl;
 
+
+
+
+    // string some function
     // concatenate strings with the append() function
     // string fullName = firstName.append(lastName);
     // string fullName = firstName.append("Ahmed");
     // cout << fullName << endl;
 
-    // best practice recommended by w3 schools
-    string fullName = firstName;   // আগে copy
-    fullName.append("Ahmed");     // তারপর append
-    cout << fullName << endl;
 
-    // string some function
-    // string.at()
+
+
+    // returns an indexed character from a string.
+    // string txt = "Hello World!";
+    // cout << txt.at(0) << endl;
+
+    // access the last character by technique
+    // cout << txt.at(txt.length() - 1);
+
+
+
+
+    // get the last character of string
+    // string txt = "Hello World!";
+    // cout << txt.back() << endl;
+
+    
+
+
+    // Checks whether the string is empty
+    // string txt1 = "Hello World!";
+    // string txt2 = "";
+    // cout << txt1.empty() << endl; // 0
+    // cout << txt2.empty() << endl; / 1
+
+
+
+
+    // Returns the number of characters in the string
+    // string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // cout << "The length of the txt string is: " << txt.length() << endl;
+    // cout << "The length of the txt string is: " << txt.size();
+
+
+
+
+
 
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,2 @@
- // bool isCodingFun = true;
-    // bool isFishTasty = false;
-    // cout << isCodingFun;  // Outputs 1 (true)
-    // cout << isFishTasty;  // Outputs 0 (false)
+= "Hello World!";
+    // cout << txt.back();
