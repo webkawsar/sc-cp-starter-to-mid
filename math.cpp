b574d88a2dc215e2ae2e378132c45287355006c2
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-13 23:07:58 +0600

    practice math functions

---
+++ math.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+// Math
+
+
+int main() {
+
+    // get max value
+    // cout << max(5, 10) << endl;
+
+    // get min value
+    // cout << min(5, 10) << endl;
+
+
+
+
+    // cmath library functions
+    // cout << sqrt(64) << endl;
+    // cout << round(2.6) << endl;
+    // cout << round(2.4) << endl;
+    // cout << log(2) << endl;
+
+
+    // Return the absolute (positive) value
+    // cout << abs(-4.7) << endl;
+    // cout << abs(4.7) << endl;
+    // cout << abs(3) << endl;
+
+    // cout << fabs(-4.7) << "\n";
+    // cout << fabs(4.7) << "\n";
+    // cout << fabs(3) << "\n";
+
+
+
+
+
+    // cout << ceil(0.60) << "\n";
+    // cout << ceil(0.40) << "\n";
+    // cout << ceil(5) << "\n";
+    // cout << ceil(5.1) << "\n";
+    // cout << ceil(-5.1) << "\n";
+    // cout << ceil(-5.9) << "\n";
+
+
+
+    // cout << floor(0.60) << "\n";
+    // cout << floor(0.40) << "\n";
+    // cout << floor(5) << "\n";
+    // cout << floor(5.1) << "\n";
+    // cout << floor(-5.1) << "\n";
+    // cout << floor(-5.9) << "\n";
+
+
+
+
+    // cout << fmax(2.0, 0.25) << "\n";
+    // cout << fmax(31.2f, 18.0f) << "\n";
+    // cout << fmax(14, 22) << "\n";
+    // cout << fmax(96, 2048) << "\n";
+
+    // cout << fmin(2.0, 0.25) << "\n";
+    // cout << fmin(31.2f, 18.0f) << "\n";
+    // cout << fmin(14, 22) << "\n";
+    // cout << fmin(96, 2048) << "\n";
+
+
+    // cout << pow(2, 3) << "\n";
+
+
+    // 0.50
+    // cout << round(0.60) << "\n";
+    // cout << round(0.40) << "\n";
+    // cout << round(5) << "\n";
+    // cout << round(5.1) << "\n";
+    // cout << round(-5.1) << "\n";
+    // cout << round(-5.9) << "\n";
+
+
+
+    // cout << sqrt(0) << "\n";
+    // cout << sqrt(1) << "\n";
+    // cout << sqrt(25) << "\n";
+    // cout << sqrt(9) << "\n";
+    // cout << sqrt(0.64) << "\n";
+    // cout << sqrt(-64) << "\n";
+    // cout << sqrt(-25) << "\n";
+
+
+    // cout << trunc(0.60) << "\n";
+    // cout << trunc(0.40) << "\n";
+    // cout << trunc(5) << "\n";
+    // cout << trunc(5.1) << "\n";
+    // cout << trunc(-5.1) << "\n";
+    // cout << trunc(-5.98765) << "\n";
+
+}
