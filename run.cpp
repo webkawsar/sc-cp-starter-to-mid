9888378d14284c0cfadee41a5b41b861fd253bc1
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-16 23:52:44 +0600

    practice some problems

--- run.cpp
+++ run.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main() {
-  int age = 15;
+  int testCase;
+    cin >> testCase;
+
+    while (testCase > 0) {
+      int question;
+      cin >> question;
+
+      if (question <= 10) {
+          cout << 0 << " " << question << "\n";
+      } else {
+          cout << 10 << " " << (question - 10) << "\n";
+      }
+
+      testCase--;
+    }
+  
   
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,23 @@
-(time >= 18) ? "You can vote" : "You are not a voter"
+#include <iostream>
+using namespace std;
+
+int main() {
+  int testCase;
+  cin >> testCase;
+
+  int mountainCount;
+  cin >> mountainCount;
+
+  int tallestMountain;
+  long long mountains[mountainCount];
+
+  for(int i = 0; i < mountainCount; i++) {
+    cin >> mountains[i];
+    if(mountains[i] > tallestMountain) {
+      tallestMountain = mountains[i];
+    }
+  }
+
+  cout << tallestMountain;
+
+}
