889c08e35f4ca6f77d278cecbdc4ea1ee08df383
webkawsar <web.kawsarahmed@gmail.com>
2026-01-22 12:42:06 +0600

    practice structure

---
+++ struct.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  
+  struct {
+  	int id;
+    string name;
+    int age;
+    string profession;
+  } dev, developer, programmer;
+  
+  dev.id = 101;
+  dev.name = "Kawsar AHmed";
+  dev.age = 30;
+  dev.profession = "Developer";
+  
+  cout << dev.id << endl;
+  cout << dev.name << endl;
+  cout << dev.age << endl;
+  cout << dev.profession << endl;
+  
+  programmer.id = 102;
+  programmer.name = "Samim Mohammed";
+  programmer.age = 30;
+  programmer.profession = "Pilot";
+  
+  cout << "===============================" << endl;
+  cout << programmer.id << endl;
+  cout << programmer.name << endl;
+  cout << programmer.age << endl;
+  cout << programmer.profession << endl;
+  
+  return 0;
+}
--- test.php
+++ test.php
@@ -7,252 +7,153 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<meta name="keywords" content="nedjon media, nedjon, music, nigeria, mp3, lagos, gospel music, nigerian, blue pie records, dj central records, pop, gospel music, youtube music, streaming, spotify">
+  <meta name="keywords"
+    content="nedjon media, nedjon, music, nigeria, mp3, lagos, gospel music, nigerian, blue pie records, dj central records, pop, gospel music, youtube music, streaming, spotify">
 
-<title><?php bloginfo('name'); ?></title>
+  <title><?php bloginfo('name'); ?></title>
 
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-<?php wp_head(); ?>
-<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+  <?php wp_head(); ?>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
 
-<style>
-.fit-vids-style{
-    margin-bottom: -30px;
-}
-body.loading{ overflow:hidden; }
-body.loading .container{ visibility:hidden; }
-
-.wrapper,
-.container{ position:relative; }
-
-.header{
-  position:relative;
-  width:100%;
-  z-index:1000;
-  display:block;
-}
-
-.topHeader{
-  display:flex;
-  flex-direction: column;
-}
-
-/*
-.navbar-toggle .icon-bar{
-  display:block;
-  width:26px;
-  height:3px;
-  margin:5px 0;
-  background:#fff;
-} */
-
-
-.slider{
-  position:relative;
-  width:100%;
-  z-index:1;
-}
-
-
-/* 
-@media (max-width:1199px){
-.latincentralrecords-hero-image {
-    position: relative;
-    width: 100%;
-    overflow: hidden;
-    margin-top: -160px;
-}
-  .topHeader{
-    flex-wrap:wrap;
-    gap:10px;
-  }
-
-
-  .topHeader .logo{
-    order:1;
-    flex:1 1 auto;
-    display:flex;
-    align-items:center;
-  }
-
-  .topHeader .menu{
-    order:2;
-    flex:0 0 auto;
-  }
-
-  .navbar-toggle{ display:block; }
-
-
-  .topHeader .menu.is-open{
-    order:3;
-    width:100%;
-    flex:1 1 100%;
-  }
-
-  .topHeader .menu ul.nav-menu{
-    display:none;
-    width:100%;
-    background:#000;
-    margin-top:8px;
-    padding:0;
-    list-style:none;
-  }
-
-  .topHeader .menu.is-open ul.nav-menu{
-    display:block;
-  }
-
-  .topHeader .menu ul.nav-menu li{
-    border-top:1px solid rgba(255,255,255,.15);
-  }
-
-  .topHeader .menu ul.nav-menu li:first-child{
-    border-top:0;
-  }
-
-  .topHeader .menu ul.nav-menu a{
-    display:block;
-    padding:12px 15px;
-    text-decoration:none;
-  }
-
-  .topHeader > .social-media{
-    order:4;
-    width:100%;
-    display:flex;
-    justify-content:flex-end;
-    margin-top:6px;
-    gap:8px;
-  }
-} */
-
-/* @media (max-width:1023px){
-    .topHeader > .social-media {
-      margin-top: -155px;
-   }
-    
-   .topHeader .menu {
-     margin-top: -46px;
-   }
-   .header .logo img {
-   margin-top: -40px;
-}
-} */
-
-/* @media (max-width:767px){
-   .topHeader .menu {
-     margin-top: 4px;
-     z-index: 22222;
-   }
-   .topHeader > .social-media {
-        margin-top: -55px;
+  <style>
+    .fit-vids-style {
+      margin-bottom: -30px;
     }
-} */
 
-.show_only_mobile_screen {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 0;
-}
-
- 
-</style>
-
-<script>
-(function($){
-
-  function setSliderOffset(){
-    var w = window.innerWidth;
-    if(w <= 1199){
-      var headerH = $('.header').outerHeight(true);
-      $('.slider').css('margin-top', headerH + 'px');
-    }else{
-      $('.slider').css('margin-top','0');
+    body.loading {
+      overflow: hidden;
     }
-  }
 
-  function menuInit(){
-    var $menu = $('.topHeader .menu');
-    var $btn  = $('.navbar-toggle');
-    var $nav  = $('.topHeader ul.nav-menu');
+    body.loading .container {
+      visibility: hidden;
+    }
 
-    $btn.on('click', function(e){
-      e.preventDefault();
-      $menu.toggleClass('is-open');
-      $nav.stop(true,true).slideToggle(200, setSliderOffset);
-    });
+    .wrapper,
+    .container {
+      position: relative;
+    }
 
-    function sync(){
-      if(window.innerWidth > 1199){
-        $menu.removeClass('is-open');
-        $nav.show();
-      }else{
-        if(!$menu.hasClass('is-open')) $nav.hide();
+
+    .slider {
+      position: relative;
+      width: 100%;
+      z-index: 1;
+    }
+
+    .menu_container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    @media only screen and (min-width: 1024px) and (max-width: 1199px) {
+      .menu_container {
+        align-items: flex-end;
       }
-      setSliderOffset();
     }
 
-    $(window).on('resize', sync);
-    sync();
-  }
+    @media only screen and (min-width: 1200px) {
+      .menu_container {
+        flex-direction: row
+      }
+    }
+  </style>
 
-  $(document).ready(function(){
-    menuInit();
-    setSliderOffset();
-    $('body').removeClass('loading');
-  });
+  <script>
+    (function ($) {
 
-  $(window).on('load', function(){
-    setSliderOffset();
-    $('body').removeClass('loading');
-  });
+      function setSliderOffset() {
+        var w = window.innerWidth;
+        if (w <= 1199) {
+          var headerH = $('.header').outerHeight(true);
+          // $('.slider').css('margin-top', headerH + 'px');
+        } else {
+          $('.slider').css('margin-top', '0');
+        }
+      }
 
-})(jQuery);
-</script>
+      function menuInit() {
+        var $menu = $('.topHeader .menu');
+        var $btn = $('.navbar-toggle');
+        var $nav = $('.topHeader ul.nav-menu');
+
+        $btn.on('click', function (e) {
+          e.preventDefault();
+          $menu.toggleClass('is-open');
+        });
+
+        function sync() {
+          if (window.innerWidth >= 768) {
+            $menu.removeClass('is-open');
+            $nav.show();
+          } else {
+            $nav.hide();
+          }
+        }
+
+        $(window).on('resize', sync);
+        sync();
+      }
+
+
+      $(document).ready(function () {
+        menuInit();
+        setSliderOffset();
+        $('body').removeClass('loading');
+      });
+
+      $(window).on('load', function () {
+        setSliderOffset();
+        $('body').removeClass('loading');
+      });
+
+    })(jQuery);
+  </script>
 
 </head>
 
 <body class="loading">
 
-<div class="wrapper">
-<div class="container">
+  <div class="wrapper">
+    <div class="container">
 
-  <!-- HEADER -->
-  <div class="header">
-    <div class="topHeader">
-      <div class="logo">
-        <a href="<?php echo esc_url(home_url('/')); ?>">
-          <img src="/wp-content/uploads/2018/09/logo.png" alt="Nedjon Media">
-        </a>
+      <!-- HEADER -->
+      <div class="header">
+        <div class="topHeader">
+          <div class="logo">
+            <a href="<?php echo esc_url(home_url('/')); ?>">
+              <img src="/wp-content/uploads/2018/09/logo.png" alt="Nedjon Media">
+            </a>
+          </div>
+
+          <div class="menu_container">
+            <div class="menu">
+              <button class="navbar-toggle">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+              <?php wp_nav_menu(['menu' => 'Top_menu', 'menu_class' => 'nav-menu']); ?>
+            </div>
+
+            <div class="social-media">
+              <?php include('social-icons.php'); ?>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div class="show_only_mobile_screen">
-        <div class="menu">
-          <button class="navbar-toggle">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          
-          <?php wp_nav_menu(['menu'=>'Top_menu','menu_class'=>'nav-menu']); ?>
-        </div>
-
-        <div class="social-media">
-          <?php include('social-icons.php'); ?>
-        </div>
+      <!-- SLIDER -->
+      <div class="slider">
+        <?php include('slider.php'); ?>
       </div>
-    </div>
-  </div>
-
-  <!-- SLIDER -->
-  <div class="slider">
-    <?php include('slider.php'); ?>
-  </div>
