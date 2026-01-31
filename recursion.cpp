beb7bc4ee0969dd62ab55c99dd3322f4525b67cd
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-01-31 17:35:00 +0600

    practice recursion

--- recursion.cpp
+++ recursion.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 // recursion function
 
+void sum(int a) {
+    if(a==0) return;
+    cout << a << "\n";
+    sum(a-1);
+}
+
 int main() {
    
-
+    int n = 5;
+    sum(n);
 }
